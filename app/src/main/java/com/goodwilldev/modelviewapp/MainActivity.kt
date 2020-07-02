309032e2c978ed773785e5ab5c13650343cc919e
work-kiku1007 <work.sourov@gmail.com>
2020-07-02 20:49:23 +0530

    old changes

--- app/src/main/java/com/goodwilldev/modelviewapp/MainActivity.kt
+++ app/src/main/java/com/goodwilldev/modelviewapp/MainActivity.kt
@@ -10,22 +10,9 @@ import com.goodwilldev.modelviewapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-//        viewModel.getCount().observe(this, Observer {it_->
-//            text.text = it_.toString();
-//        })
-
-        binding.lifecycleOwner = this
-        binding.viewmodel = viewModel
-        button.setOnClickListener {
-            //view model
-            viewModel.SetCount()
-        }
-
+        setContentView(R.layout.activity_main)
     }
 }
